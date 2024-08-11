e5ffd2053e8babcdf095c2b1a7d1e86a5f614c5a
Reynaldhi <reynaldig40@gmail.com>
2024-08-11 21:18:46 +0700

    add firebase functionality, realtime database is working!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:standby_capstone/firebase_options.dart';
 import 'package:standby_capstone/screens/login_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const StandBy());
 }
 
--- lib/screens/monitoring_page.dart
+++ lib/screens/monitoring_page.dart
@@ -1,3 +1,7 @@
+import 'dart:ffi';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:standby_capstone/constants.dart';
 import 'package:standby_capstone/components/card_info.dart';
@@ -10,6 +14,32 @@ class MonitoringPage extends StatefulWidget {
 }
 
 class _MonitoringPageState extends State<MonitoringPage> {
+  String _currentIncubatorTemp = '0';
+  String _currentIncubatorHumi = '0';
+  String _currentSkinTemp = '0';
+  String _currentFanStatus = 'OFF';
+
+  final _database = FirebaseDatabase.instance.ref();
+
+  @override
+  void initState() {
+    super.initState();
+    _readData();
+  }
+
+  void _readData() {
+    _database.child('sensor_data').onValue.listen((event) {
+      final data = event.snapshot.value as Map<dynamic, dynamic>;
+      setState(() {
+        _currentIncubatorTemp =
+            data['DHT22']?['temperature']?.toString() ?? '0';
+        _currentIncubatorHumi = data['DHT22']?['humidity']?.toString() ?? '0';
+        _currentSkinTemp = data['DS18B20']?['temperature']?.toString() ?? '0';
+        _currentFanStatus = data['fan_status']?.toString() ?? 'OFF';
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,31 +47,31 @@ class _MonitoringPageState extends State<MonitoringPage> {
         child: Container(
           height: MediaQuery.of(context).size.height,
           color: kGray,
-          child: const Padding(
-            padding: EdgeInsets.all(24),
+          child: Padding(
+            padding: const EdgeInsets.all(24),
             child: Column(
               children: [
                 InfoCard(
                   icon: Icons.device_thermostat,
-                  title: '33°C',
+                  title: '$_currentIncubatorTemp°C',
                   subtitle: 'Suhu\nInkubator Bayi',
                 ),
-                SizedBox(height: 24),
+                const SizedBox(height: 24),
                 InfoCard(
                   icon: Icons.water_drop,
-                  title: '96%',
+                  title: '$_currentIncubatorHumi%',
                   subtitle: 'Kelembaban\nInkubator Bayi',
                 ),
-                SizedBox(height: 24),
+                const SizedBox(height: 24),
                 InfoCard(
                   icon: Icons.child_care,
-                  title: '36°C',
+                  title: '$_currentSkinTemp°C',
                   subtitle: 'Suhu\nKulit Bayi',
                 ),
-                SizedBox(height: 24),
+                const SizedBox(height: 24),
                 InfoCard(
                   icon: Icons.heat_pump,
-                  title: 'OFF',
+                  title: _currentFanStatus,
                   subtitle: 'Kipas\nPendingin',
                 ),
               ],
