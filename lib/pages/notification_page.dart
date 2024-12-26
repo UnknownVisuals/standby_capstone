f4b9cbc72f01260899684f073005b2d85130b1f1
Reynaldhi <reynaldig40@gmail.com>
2024-12-26 10:30:27 +0700

    revamp notifications page, implement filter and scroll fetching

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -13,23 +13,84 @@ class NotificationPage extends StatefulWidget {
 
 class _NotificationPageState extends State<NotificationPage> {
   List<Map<String, dynamic>> _notifications = [];
+  bool _isFetchingMore = false;
   bool _isLoading = true;
+  int _filter = 0;
+  int _currentOffset = 0;
+  final int _limit = 25;
+  final ScrollController _scrollController = ScrollController();
 
-  Future<void> _fetchNotifications() async {
+  Future<void> _fetchNotifications({bool loadMore = false}) async {
+    if (loadMore) {
+      setState(() => _isFetchingMore = true);
+    } else {
+      setState(() {
+        _isLoading = true;
+        _notifications = [];
+        _currentOffset = 0;
+      });
+    }
+
     try {
-      final response = await supabase
-          .from('notifications')
-          .select('id, created_at, temp, anomaly, anomaly_score')
-          .order('created_at', ascending: false);
+      List<Map<String, dynamic>> response;
+
+      if (_filter == 0) {
+        response = await supabase
+            .from('notifications')
+            .select('id, created_at, temp, anomaly, anomaly_score')
+            .order('created_at', ascending: false)
+            .range(_currentOffset, _currentOffset + _limit - 1);
+      } else if (_filter == 1) {
+        response = await supabase
+            .from('notifications')
+            .select('id, created_at, temp, anomaly, anomaly_score')
+            .eq('temp', 85.0)
+            .order('created_at', ascending: false)
+            .range(_currentOffset, _currentOffset + _limit - 1);
+      } else if (_filter == 2) {
+        response = await supabase
+            .from('notifications')
+            .select('id, created_at, temp, anomaly, anomaly_score')
+            .neq('temp', 85.0)
+            .order('created_at', ascending: false)
+            .range(_currentOffset, _currentOffset + _limit - 1);
+      } else if (_filter == 3) {
+        response = await supabase
+            .from('notifications')
+            .select('id, created_at, temp, anomaly, anomaly_score')
+            .not('anomaly', 'is', null)
+            .order('created_at', ascending: false)
+            .range(_currentOffset, _currentOffset + _limit - 1);
+      } else {
+        throw Exception('Invalid filter selected');
+      }
 
       setState(() {
-        _notifications = List<Map<String, dynamic>>.from(response);
+        _notifications.addAll(List<Map<String, dynamic>>.from(response));
         _isLoading = false;
+        _isFetchingMore = false;
+        _currentOffset += _limit;
       });
     } catch (error) {
       setState(() {
         _isLoading = false;
+        _isFetchingMore = false;
       });
+    }
+  }
+
+  void _changeFilter(int? filter) {
+    setState(() {
+      _filter = filter!;
+    });
+    _fetchNotifications();
+  }
+
+  void _onScroll() {
+    if (_scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent &&
+        !_isFetchingMore) {
+      _fetchNotifications(loadMore: true);
     }
   }
 
@@ -37,90 +98,148 @@ class _NotificationPageState extends State<NotificationPage> {
   void initState() {
     super.initState();
     _fetchNotifications();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kGray,
       appBar: const DeepMenuAppbar(title: 'Notifications'),
-      body: Container(
-        color: kGray,
-        height: double.infinity,
-        child: _isLoading
-            ? const Center(
-                child: CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation(kPrimary),
-                ),
-              )
-            : SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(24),
-                  child: Column(
-                    children: _generateExpansionTiles(),
-                  ),
-                ),
-              ),
-      ),
-    );
-  }
-
-  List<Widget> _generateExpansionTiles() {
-    if (_notifications.isEmpty) {
-      return [
-        Center(
-          child: Text('No notifications available.', style: kTextHeading_Red),
-        )
-      ];
-    }
-
-    return _notifications.map((data) {
-      final temp = data['temp'];
-      final anomaly = data['anomaly'];
-      final anomalyScore = data['anomaly_score'];
-      final date = DateFormat('dd/MM/yyyy - kk:mm:ss').format(
-        DateTime.parse(data['created_at']).toLocal(),
-      );
-
-      String title;
-      String content;
-
-      if (temp != null) {
-        title = 'Temperature Warning';
-        content =
-            '${temp.toStringAsFixed(2)}°C is outside suitable range for Incubator';
-      } else if (anomaly != null) {
-        title = 'Anomaly Warning';
-        content =
-            'Check your hardware device! Anomaly score: ${anomalyScore?.toStringAsFixed(2)}';
-      } else {
-        title = 'Unknown Notification';
-        content = 'No details available.';
-      }
-
-      return Container(
-        margin: const EdgeInsets.only(bottom: 24),
-        decoration: kEmbossDecoration,
-        child: Theme(
-          data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-          child: ExpansionTile(
-            iconColor: kPrimary,
-            collapsedIconColor: kPrimary,
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(title, style: kTextHeading_Red),
-                Text(date, style: kTextNormal_Black),
-              ],
-            ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ListTile(
-                dense: true,
-                title: Text(content, style: kTextNormal_Black),
+              Text('Filter: ', style: kTextHeading_Black),
+              const SizedBox(width: 12),
+              DropdownButton<int>(
+                dropdownColor: kGray,
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                alignment: AlignmentDirectional.center,
+                elevation: 4,
+                value: _filter,
+                items: [
+                  DropdownMenuItem(
+                    value: 0,
+                    child: Center(
+                      child: Text('All Notifications', style: kTextHeading_Red),
+                    ),
+                  ),
+                  DropdownMenuItem(
+                    value: 1,
+                    child: Center(
+                      child: Text('Device On 🟢', style: kTextHeading_Red),
+                    ),
+                  ),
+                  DropdownMenuItem(
+                    value: 2,
+                    child: Center(
+                      child: Text('Temperature Warning ⚠️',
+                          style: kTextHeading_Red),
+                    ),
+                  ),
+                  DropdownMenuItem(
+                    value: 3,
+                    child: Center(
+                      child:
+                          Text('Anomaly Detected ⚠️', style: kTextHeading_Red),
+                    ),
+                  ),
+                ],
+                onChanged: _changeFilter,
               ),
             ],
           ),
-        ),
-      );
-    }).toList();
+          Expanded(
+            child: _isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation(kPrimary),
+                    ),
+                  )
+                : ListView.builder(
+                    controller: _scrollController,
+                    itemCount:
+                        _notifications.length + (_isFetchingMore ? 1 : 0),
+                    itemBuilder: (context, index) {
+                      if (index == _notifications.length && _isFetchingMore) {
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation(kPrimary),
+                          ),
+                        );
+                      }
+
+                      final data = _notifications[index];
+
+                      final date = DateFormat('dd/MM/yyyy - kk:mm:ss').format(
+                        DateTime.parse(data['created_at']).toLocal(),
+                      );
+
+                      final temp = data['temp'];
+                      final anomaly = data['anomaly'];
+                      final anomalyScore = data['anomaly_score'];
+
+                      String title;
+                      String content;
+
+                      if (temp != null && temp == 85.0) {
+                        title = 'Device On 🟢';
+                        content =
+                            'Device is on and ready for real-time monitoring!';
+                      } else if (temp != null) {
+                        title = 'Temperature Warning ⚠️';
+                        content =
+                            '${temp.toStringAsFixed(2)}°C is outside the suitable range for the incubator.';
+                      } else if (anomaly != null) {
+                        title = 'Anomaly Detected ⚠️';
+                        content =
+                            'Check your hardware device! Anomaly score: ${anomalyScore?.toStringAsFixed(2)}';
+                      } else {
+                        title = 'Unknown Notification';
+                        content = 'No details available.';
+                      }
+
+                      return Container(
+                        padding: const EdgeInsets.all(12),
+                        child: Theme(
+                          data: Theme.of(context).copyWith(
+                            dividerColor: Colors.transparent,
+                          ),
+                          child: ExpansionTile(
+                            iconColor: kPrimary,
+                            collapsedIconColor: kPrimary,
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(title, style: kTextHeading_Red),
+                                Text(date, style: kTextNormal_Black),
+                              ],
+                            ),
+                            children: [
+                              ListTile(
+                                dense: true,
+                                title: Text(
+                                  content,
+                                  style: kTextNormal_Black,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
+      ),
+    );
   }
 }
